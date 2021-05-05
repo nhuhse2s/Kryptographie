3c1d541fa23cd2c8ab7a53c5d33a72509660dea9
nhuhse2s <nils.huhse@smail.inf.h-brs.de>
2021-05-05 14:56:37 +0200

    - unsupported operand type(s) for ^=: 'bytes' and 'bytes'

--- 02/c1.py
+++ 02/c1.py
@@ -7,19 +7,32 @@ import numpy as np
 def Decrypt(ciphertext, key):
     counter = 0
     l = []
-    keyList = []
+    keyBytes = []
+    
     for i in key.tolist():
-        keyList.append(i.to_bytes)
+        keyBytes.append(i.to_bytes(1, "little", signed=True))
 
-    print(keyList)
-    #print(type(keyList1))
-    #keyBytes = [abs(i) for i in key]
-    #print(keyBytes)
-    for i in ciphertext:
-        l.append(i ^ (key[counter]))    ## XOR operation
-        counter += 1
-    #result = [abs(i) for i in l]
-    return l
+
+    a = (keyBytes[0])
+    b = (ciphertext[0].to_bytes(1, byteorder="little"))
+    print((keyBytes[0]))
+    print("test", ciphertext[0])
+    print((ciphertext[0].to_bytes(1, byteorder="little")))
+
+    bytesCipherText = []
+    for number in ciphertext:
+        bytesCipherText.append(number.to_bytes(1, byteorder="little"))
+
+    print(bytesCipherText)
+    #result = bytearray(bytesCipherText)
+    #for counter, key in enumerate(keyBytes):
+     #   bytesCipherText[counter] ^= key
+
+    # for i in ciphertext:
+    #     l.append(i.to_bytes(1, "little") ^ keyList[counter])    ## XOR operation
+    #     counter += 1
+
+    return 
 
 def Main():
     c = open("data/chiffratbin.sec", "rb").read()
