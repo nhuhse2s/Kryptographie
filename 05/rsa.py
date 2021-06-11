e5a3b7bbd786f91f95bb7940225577d682d4d27c
nhuhse2s <nils.huhse@smail.inf.h-brs.de>
2021-06-11 17:34:29 +0200

    encryption & decryption fix

--- 05/rsa.py
+++ 05/rsa.py
@@ -1,5 +1,13 @@
+###############################
+### RSA Key Generation, Encryption & Decryption
+###############################
 from Crypto.Util import number
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 from timeit import default_timer as timer
+import Crypto.Math
+import random
+import math as m
 
 def RSA_a():  
     prime_length = 1500
@@ -10,17 +18,22 @@ def RSA_a():
         print("x: ", x, "| time: ", end - start, " Sekunden")
 
 def RSA_b():
-    prime_length = 100
-    ggt = 0
-    while(ggt != 1):
-        prime_p      = number.getPrime(prime_length)    ##5
-        prime_q      = number.getPrime(prime_length)    ##11
-        n            = prime_p*prime_q                  ##55
-        phi_n        = (prime_p-1)*(prime_q-1)          ##40
-        e            = (2**16)+1                               ##17, (2**16) + 1
-        ggt = GGT(e, phi_n)
-        print("test", ggt)
-    d = ModInv(e, phi_n)                 ##33
+    prime_length = 3000
+    ggt          = 0
+    prime_p      = number.getPrime(prime_length)    
+    prime_q      = number.getPrime(prime_length)
+    n            = prime_p*prime_q                  
+    phi_n        = (prime_p-1)*(prime_q-1)          
+    #e            = (2**16)+1
+    while True:
+      e = random.randrange(2 ** (prime_length - 1), 2 ** (prime_length))
+      if m.gcd(e, phi_n) == 1 & (e < phi_n):
+         break
+                                     
+    d = pow(e, -1, phi_n)
+    #d1 = Crypto.Math.findModInverse(e, phi_n)
+    #print("d: ",d)
+    #print("d1: ",d1)                
         
     # prime_p      = 5    
     # prime_q      = 11    
@@ -28,7 +41,7 @@ def RSA_b():
     # phi_n        = 40         
     # e            = 17                      
     # d            = modinv(e, phi_n)                 
-    return (n, e, d)
+    return prime_p, prime_q, phi_n, n, e, d
 
 def RSA_encrypt(x, e, n):
     return (x**e)%n
@@ -53,15 +66,38 @@ def GGT(a, b):
     return a
 
 if __name__ == "__main__":
-    #RSA_a()
-    
-    n, e, d = RSA_b()
-    print("n: ", n)
-    print("e: ", e)
-    print("d: ", d)
-    message = 30
-    m_encrypted = RSA_encrypt(message, e, n)
 
-    print("Message: ", message)
-    print("RSA_encrypt: ", m_encrypted)
-    print("RSA_decrypt: ", RSA_decrypt(m_encrypted, d, n))
+    #RSA_a()
+
+    p, q, phi_n, n, e, d = RSA_b()
+    # print("p: ", p)
+    # print("q: ", q)
+    # print("phi: ", phi_n)
+    # print("n: ", n)
+    # print("e: ", e)
+    # print("d: ", d)
+    
+    message = b'helloworld'
+    print("----building keys------")
+    public_key = RSA.construct((n, e))
+    private_key = RSA.construct((n, d))
+    key_pair = RSA.construct((n, e, d,))
+    print("PublicKey: ", public_key)
+    print("-----------------------")
+    print("PrivateKey: ", key_pair)
+    print("-----------------------")
+    print("Original message: ", message)
+    print("-----------------------")
+    encryptor = PKCS1_OAEP.new(public_key)
+    m_encrypted = encryptor.encrypt(message)
+    print("Encrypted message: ", m_encrypted,"\n")
+    print("-----------------------")
+    decryptor = PKCS1_OAEP.new(key_pair)
+    m_decrypted = decryptor.decrypt(m_encrypted)
+    print("Decrypted message: ", m_decrypted)
+    print("-----------------------")
+
+    # m_encrypted = RSA_encrypt(message, e, n)
+    # print("Message: ", message)
+    # print("RSA_encrypt: ", m_encrypted)
+    # print("RSA_decrypt: ", RSA_decrypt(m_encrypted, d, n))
