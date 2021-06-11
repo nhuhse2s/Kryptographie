eab1fdd80eabe2c478cca167c7fb451919c500d4
nhuhse2s <nils.huhse@smail.inf.h-brs.de>
2021-06-11 14:20:39 +0200

    ggt condition

--- 05/rsa.py
+++ 05/rsa.py
@@ -10,14 +10,18 @@ def RSA_a():
         print("x: ", x, "| time: ", end - start, " Sekunden")
 
 def RSA_b():
-    prime_length = 500
-    prime_p      = number.getPrime(prime_length)    ##5
-    prime_q      = number.getPrime(prime_length)    ##11
-    n            = prime_p*prime_q                  ##55
-    phi_n        = (prime_p-1)*(prime_q-1)          ##40
-    e            = (2**16) + 1                      ##17
-    d            = modinv(e, phi_n)                 ##33
-
+    prime_length = 100
+    ggt = 0
+    while(ggt != 1):
+        prime_p      = number.getPrime(prime_length)    ##5
+        prime_q      = number.getPrime(prime_length)    ##11
+        n            = prime_p*prime_q                  ##55
+        phi_n        = (prime_p-1)*(prime_q-1)          ##40
+        e            = (2**16)+1                               ##17, (2**16) + 1
+        ggt = GGT(e, phi_n)
+        print("test", ggt)
+    d = ModInv(e, phi_n)                 ##33
+        
     # prime_p      = 5    
     # prime_q      = 11    
     # n            = 55                 
@@ -32,7 +36,7 @@ def RSA_encrypt(x, e, n):
 def RSA_decrypt(x, d, n):
     return (x**d)%n
 
-def modinv(e, phi):
+def ModInv(e, phi):
     d_old = 0; r_old = phi
     d_new = 1; r_new = e
     while r_new > 0:
@@ -41,10 +45,20 @@ def modinv(e, phi):
         (r_old, r_new) = (r_new, r_old - a * r_new)
     return d_old % phi if r_old == 1 else None
 
+def GGT(a, b):
+    while b!=0:
+        c=a%b
+        a=b
+        b=c
+    return a
+
 if __name__ == "__main__":
     #RSA_a()
     
     n, e, d = RSA_b()
+    print("n: ", n)
+    print("e: ", e)
+    print("d: ", d)
     message = 30
     m_encrypted = RSA_encrypt(message, e, n)
 
